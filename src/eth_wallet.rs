e13abd58465fbc9106be8137b578257e809ff7a6
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-28 12:58:59 +0530

    Wallet struct added

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -7,6 +7,24 @@ use std::io::BufWriter;
 use std::str::FromStr;
 use std::{fs::OpenOptions, io::BufReader};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+}
+
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(111);
