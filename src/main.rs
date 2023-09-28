7aa1861ff8055321fc696f25c7357a0b48ce9b29
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-28 13:01:27 +0530

    created eth_wallet::Wallet

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ mod eth_wallet;
 fn main() {
     let (secret_key, pub_key) = eth_wallet::generate_keypair();
 
-    println!("Secret key: {}", secret_key.to_string());
-    println!("Public Key: {}", pub_key.to_string());
+    println!("Secret key: {}", &secret_key.to_string());
+    println!("Public Key: {}", &pub_key.to_string());
 
     let pub_address = eth_wallet::public_key_address(&pub_key);
     println!("Address: {}", pub_address.to_string());
+
+    let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
+    println!("Crypto wallet: {:?}", &crypto_wallet);
 }
